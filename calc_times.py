b93a6f54268224b6d3c59529bdc6b64931162d62
ardubev16 <43483037+ardubev16@users.noreply.github.com>
2022-09-24 16:04:19 +0200

    refactored times

--- calc_times.py
+++ calc_times.py
@@ -5,24 +5,18 @@ import time
 with open("times.log") as f:
     times = f.readlines()
 
-names = ["Bevilacqua", "Sartore", "Tecchio"]
+names = {"B": "Bevilacqua", "S": "Sartore", "T": "Tecchio"}
 
 total_times = {}
-total_all = 0
 for time_entry in times:
-    [name, time_range, _] = time_entry.split("; ")
+    [initials, time_range, _] = time_entry.split("; ")
     [start, end] = time_range.split("-")
     start = time.strptime(start, "%H.%M")
     end = time.strptime(end, "%H.%M")
     total_time = time.mktime(end) - time.mktime(start)
-    if name == "Tutti":
-        total_all += total_time
-    else:
-        total_times.update({name: total_times.get(name, 0) + total_time})
-
-for name in names:
-    total_times.update({name: total_times.get(name, 0) + total_all})
+    for char in initials:
+        total_times.update({char: total_times.get(char, 0) + total_time})
 
 for key in total_times:
     formatted_time = time.strftime("%H:%M", time.gmtime(total_times[key]))
-    print("{}:\t{}".format(key, formatted_time))
+    print("{}:\t{}".format(names[key], formatted_time))
