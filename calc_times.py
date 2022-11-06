ff167b4078f80b2179339854ceb45715343bdf24
ardubev16 <43483037+ardubev16@users.noreply.github.com>
2022-11-06 12:25:16 +0100

    fix times script, component diagram

--- calc_times.py
+++ calc_times.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
 
-import time
-
 with open("times.log") as f:
     times = f.readlines()
 
-names = {"B": "Bevilacqua", "S": "Sartore", "T": "Tecchio"}
+NAMES = {"B": "Bevilacqua", "S": "Sartore", "T": "Tecchio"}
+
+
+def get_minutes(start, end):
+    [sh, sm] = start.split(":")
+    [eh, em] = end.split(":")
+    minutes = (int(eh) - int(sh)) * 60 + (int(em) - int(sm))
+    return minutes
+
 
 total_times = {}
 for time_entry in times:
     [initials, time_range, _] = time_entry.split("; ")
     [start, end] = time_range.split("-")
-    start = time.strptime(start, "%H:%M")
-    end = time.strptime(end, "%H:%M")
-    total_time = time.mktime(end) - time.mktime(start)
+    total_time = get_minutes(start, end)
     for char in initials:
         total_times.update({char: total_times.get(char, 0) + total_time})
 
 for key in total_times:
-    formatted_time = time.strftime("%H:%M", time.gmtime(total_times[key]))
-    print("{}:\t{}".format(names[key], formatted_time))
+    formatted_time = f"{total_times[key] // 60}h {total_times[key] % 60}m"
+    print("{}:\t{}".format(NAMES[key], formatted_time))
